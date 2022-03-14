d3fe3c08f95f4832748fa3e3227ba04b5bfeccae
Jiwon <qwes8873@gmail.com>
2022-03-14 17:41:22 +0900

    Add templates and render it

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from scrap import *
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return render_template('index.html')
 
 app.run(debug=True)
